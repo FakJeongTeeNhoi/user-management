c7f0a75e4f11544936a5b03504be785cfd88b86c
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2024-09-15 17:35:28 +0700

    fix: faculty and type must not be null

--- model/account.go
+++ model/account.go
@@ -7,14 +7,14 @@ type account struct {
 	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
 	Name     string `json:"name" gorm:"not null"`
-	Faculty  string `json:"faculty"`
-	Type     string `json:"type"`
+	Faculty  string `json:"faculty" gorm:"not null"`
+	Type     string `json:"type" gorm:"not null"`
 }
 
 type accountCreateRequest struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Name     string `json:"name" binding:"required"`
-	Faculty  string `json:"faculty"`
-	Type     string `json:"type"`
+	Faculty  string `json:"faculty" binding:"required"`
+	Type     string `json:"type" binding:"required"`
 }
